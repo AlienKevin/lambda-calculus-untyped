2ce8dcab92ce568728eb13aa6d1e1d8e99a5bba5
Kevin Li <kevinli020508@gmail.com>
2020-05-16 16:10:45 -0400

    Can add cell in repl

--- src/LambdaRepl.elm
+++ src/LambdaRepl.elm
@@ -2,8 +2,14 @@ module LambdaRepl exposing (main)
 
 
 import Browser
+import Browser.Dom
 import Html exposing (Html)
 import Html.Attributes
+import Html.Events
+import Keyboard.Event exposing (KeyboardEvent)
+import Keyboard.Key
+import Task
+import Json.Decode
 import Dict exposing (Dict)
 import LambdaParser exposing (Def, Expr)
 import LambdaChecker
@@ -15,34 +21,48 @@ import Element.Font as Font
 
 
 type alias Model =
-  { defs : Dict Int (String, Result String Def)
-  , editingDefIndex : Int
+  { cells : Dict Int Cell
+  , editingCellIndex : Int
   , evalStrategy : EvalStrategy
   }
 
 
 type Msg
-  = EditDef String
+  = EditCell String
+  | HandleKeyDown KeyboardEvent
+  | NoOp
+
+
+type alias Cell =
+  (String, Result String Def)
 
 
 main : Program () Model Msg
 main =
-  Browser.sandbox
+  Browser.element
     { init = init
     , view = view
     , update = update
+    , subscriptions = subscriptions
     }
 
 
-init : Model
-init =
-  { defs =
-    Dict.singleton 0 <| ("", Err "")
-  , editingDefIndex =
-    0
-  , evalStrategy =
-    CallByValue
-  }
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( { cells =
+      Dict.singleton 0 emptyCell
+    , editingCellIndex =
+      0
+    , evalStrategy =
+      CallByValue
+    }
+  , Cmd.none
+  )
+
+
+emptyCell : Cell
+emptyCell =
+  ("", Err "")
 
 
 view : Model -> Html Msg
@@ -61,18 +81,18 @@ view model =
   ] <|
   List.indexedMap
     (\index result ->
-      viewDef model.editingDefIndex index result
+      viewCell model.editingCellIndex index result
     )
-    (Dict.values model.defs)
+    (Dict.values model.cells)
 
 
-viewDef : Int -> Int -> (String, Result String Def) -> E.Element Msg
-viewDef editingDefIndex currentDefIndex (src, result) =
+viewCell : Int -> Int -> (String, Result String Def) -> E.Element Msg
+viewCell editingCellIndex currentCellIndex (src, result) =
   let
     resultDisplay =
       case result of
         Ok def ->
-          E.text <| "   " ++ LambdaParser.showDef def
+          E.text <| "  " ++ LambdaParser.showDef def
         
         Err msg ->
           E.text <| msg
@@ -84,13 +104,17 @@ viewDef editingDefIndex currentDefIndex (src, result) =
     [ E.row
       [ E.width E.fill]
       [ E.text <| "> "
-      , if editingDefIndex == currentDefIndex then
+      , if editingCellIndex == currentCellIndex then
           Input.text
             [ E.width E.fill
             , Input.focusedOnLoad
+            , E.htmlAttribute <|
+              Html.Events.on "keydown" <|
+              Json.Decode.map HandleKeyDown Keyboard.Event.decodeKeyboardEvent
+            , E.htmlAttribute <| Html.Attributes.id <| "cell" ++ String.fromInt currentCellIndex
             ]
             { onChange =
-              EditDef
+              EditCell
             , text =
               src
             , placeholder =
@@ -99,47 +123,92 @@ viewDef editingDefIndex currentDefIndex (src, result) =
               Input.labelHidden "edit definition"
             }
         else
+          E.el
+          [ E.htmlAttribute <| Html.Attributes.id <| "cell" ++ String.fromInt currentCellIndex
+          ] <|
           E.text src
       ]
     , resultDisplay
     ]
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    EditDef newSrc ->
-      editDef newSrc model
+    EditCell newSrc ->
+      editCell newSrc model
+
+    HandleKeyDown event ->
+      handleKeyDown event model
+      
+    NoOp ->
+      (model, Cmd.none)
 
 
-editDef : String -> Model -> Model
-editDef newSrc model =
-  { model
-    | defs =
-      Dict.update
-        model.editingDefIndex
-        (\_ ->
-          let
-            otherDefs =
-              Dict.foldl
-                (\index (_, result) others ->
-                  if index /= model.editingDefIndex then
-                    case result of
-                      Ok def ->
-                        def :: others
-                      
-                      Err _ ->
-                        others
-                  else
-                    others
-                )
-                []
-                model.defs
-          in
-          Just (newSrc, evalDef model.evalStrategy otherDefs newSrc)
-        )
-        model.defs
-  }
+handleKeyDown : KeyboardEvent -> Model -> (Model, Cmd Msg)
+handleKeyDown { keyCode } model =
+  case keyCode of
+    Keyboard.Key.Enter ->
+      addCell model
+    
+    _ ->
+      (model, Cmd.none)
+
+
+addCell : Model -> (Model, Cmd Msg)
+addCell model =
+  let
+    newEditingCellIndex =
+      model.editingCellIndex + 1
+  in
+  ( { model
+      | cells =
+        Dict.foldr
+          (\index def ->
+            if index > model.editingCellIndex then
+              Dict.insert (index + 1) def
+            else
+              Dict.insert index def
+          )
+          (Dict.singleton newEditingCellIndex emptyCell)
+          model.cells
+      , editingCellIndex =
+        newEditingCellIndex
+    }
+  , Task.attempt (\_ -> NoOp) <| Browser.Dom.focus <| "cell" ++ String.fromInt newEditingCellIndex
+  )
+
+
+editCell : String -> Model -> (Model, Cmd Msg)
+editCell newSrc model =
+  ( { model
+      | cells =
+        Dict.update
+          model.editingCellIndex
+          (\_ ->
+            let
+              otherCells =
+                Dict.foldl
+                  (\index (_, result) others ->
+                    if index /= model.editingCellIndex then
+                      case result of
+                        Ok def ->
+                          def :: others
+                        
+                        Err _ ->
+                          others
+                    else
+                      others
+                  )
+                  []
+                  model.cells
+            in
+            Just (newSrc, evalDef model.evalStrategy otherCells newSrc)
+          )
+          model.cells
+    }
+  , Cmd.none
+  )
 
 
 evalDef : LambdaEvaluator.EvalStrategy -> List Def -> String -> Result String Def
@@ -156,3 +225,7 @@ evalDef strategy otherDefs src =
         problems ->
           Err <| LambdaChecker.showProblems src problems
   
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none

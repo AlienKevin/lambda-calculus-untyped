7bb1783a3c5504fb04d9537ff9d106908167717b
Kevin Li <kevinli020508@gmail.com>
2020-05-20 06:46:42 -0400

    Support bool type and if expr in parser

--- src/LambdaDebug.elm
+++ src/LambdaDebug.elm
@@ -3,49 +3,12 @@ module LambdaDebug exposing (main)
 
 import Html
 import LambdaParser
-import LambdaChecker
-import LambdaEvaluator
+-- import LambdaChecker
+-- import LambdaEvaluator
 
 
 source =
-  -- "id = \\x. x"
-  -- "always = \\a. \\b. a"
-  -- "apply = \\a. \\b. a b"
-  -- "f = \\x. \\y. x y x"
-  -- "f = x y z"
---   """{- tru in lambda calculus -}
--- v = tru fls fls
--- tru = \\a. \\b. a -- true
-
--- fls = \\a. \\b. b -- false
---   """
-  -- "v = (\\x.x) ((\\x.x) (\\z. (\\x.x) z))"
---   """
--- pair = \\f. \\s. \\b. b f s
--- fst = \\p. p tru
--- snd = \\p. p fls
--- tru = \\t. \\f. t
--- fls = \\t. \\f. f
--- v = pair tru fls
--- v2 = pair (pair tru fls) (pair fls tru)
--- c0 = \\s. \\z. z
--- c1 = \\s. \\z. s z
--- plus = \\m. \\n. \\s. \\z. m s (n s z)
--- c0Plus1 = plus c0 c1
--- c1Plus1 = plus c1 c1
--- -- divergent
--- merge = (\\x. x x) (\\x. x x)
---   """
-  -- "v = (\\f. \\s. \\b. b f s) (\\t. \\f. t) (\\t. \\f. f)"
-  -- "v = \\b. b (\\t. \\f. t) (\\t. \\f. f)"
-  """pair =
- \\f. \\s. \\a. \\b.
-   \\b. b f s
-  \\b. b f s
-"""
---   """f = \\a. a
--- b = \\x. x
---   """
+  "id = if true then \\x:(Bool-> Bool) -> Bool. x else \\y:(Bool->Bool)->Bool. y"
 
 main =
   let
@@ -66,21 +29,21 @@ main =
         Html.div []
           [ Html.h1 [] [ Html.text "Parse tree:"]
           , Html.pre [] [ Html.text <| LambdaParser.showDefs defs ]
-          , case LambdaChecker.checkDefs defs of
-            [] ->
-              let
-                resultDefs =
-                  LambdaEvaluator.evalDefs LambdaEvaluator.FullEvaluation defs
-              in
-              Html.div []
-              [ Html.h1 [] [ Html.text "Evaluation result:" ]
-              , Html.pre []
-                [ Html.text <| LambdaParser.showDefs resultDefs
-                ]
-              ]
+          -- , case LambdaChecker.checkDefs defs of
+          --   [] ->
+          --     let
+          --       resultDefs =
+          --         LambdaEvaluator.evalDefs LambdaEvaluator.FullEvaluation defs
+          --     in
+          --     Html.div []
+          --     [ Html.h1 [] [ Html.text "Evaluation result:" ]
+          --     , Html.pre []
+          --       [ Html.text <| LambdaParser.showDefs resultDefs
+          --       ]
+          --     ]
             
-            problems ->
-              Html.pre [] [ Html.text <| LambdaChecker.showProblemsWithSingleSource source problems ]
+          --   problems ->
+          --     Html.pre [] [ Html.text <| LambdaChecker.showProblemsWithSingleSource source problems ]
           ]
     ]
 
